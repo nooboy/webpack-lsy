0b8dea603721572368c24e616fdcad9173c12316
nooboy <lsyimpr@163.com>
2020-03-26 17:27:15 +0800

    去掉所有注释

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,9 +1,10 @@
+import './index.less';
 
 function component () {
   const frag = document.createDocumentFragment();
 
   const div1 = document.createElement('div');
-  div1.className = 'hello-word';
+  div1.className = 'hello-world';
   div1.innerHTML = 'Hello word haha';
   frag.appendChild(div1);
   return frag;
--- webpack.config.lsy.js
+++ webpack.config.lsy.js
@@ -1,36 +1,52 @@
-// webpack是node书写的，所以要用node语法
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',        // 两种模式：production、development
-  entry: './src/index.js',
+  mode: 'production',
+  entry: './src/pages/Index/index.js',
   output: {
-    filename: 'bundle.[hash:8].js',    // 打包后的文件名，打包后的文件带上 hash
-    path: path.resolve(__dirname,  'dist'),     // 打包后的路径，必须是一个绝对路径
+    filename: 'bundle.[hash:8].js',
+    path: path.resolve(__dirname,  'dist'),
   },
-  devServer: {              // 配置开发服务器
-    host: '127.0.0.1',      // 默认是localhost
-    // progress: true,         // 打包过程中是否显示过程
-    contentBase: './dist',  // 本地服务器所加载的页面所在的目录
-    open: true,             // 自动打开页面
-    inline: true,           // 文件有改动时自动刷新页面，实时刷新
+  devServer: {
+    host: '127.0.0.1',
+    progress: true,
+    contentBase: './dist',
+    open: true,
+    inline: true,
     port: 3000,
-    historyApiFallback: true,     // 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+    historyApiFallback: true,
     compress: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(less|sass|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              insert: 'head',
+            }
+          },
+          'css-loader',
+          'less-loader',
+          'sass-loader',
+        ]
+      },
+    ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'lsyApp',              // 指定页面标题，以 template.html 为模板，在dist目录下生成 html 文件
-      template: 'template.html',    // 指定使用的模板文件
-      // 默认是生成 filename 是 index.html，访问路径是localhost:3000或localhost:3000/index.html。
-      // 如果命名为其他名称，如 main.html，则访问路径是localhost:3000/main.html
-      filename: 'index.html',       // 生成 html 的文件名为 index.html
-      hash: true,                   // 让 html 引入的 js 或 css 末尾带上hash
-      minify: {                                 // html 进行压缩
-        collapseWhitespace: true,               // 合并空格
-        removeComments: true,                   // 去掉注释
+      title: 'lsyApp',
+      template: 'template.html',
+      filename: 'index.html',
+      hash: true,
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
         removeRedundantAttributes: true,       
         removeScriptTypeAttributes: true,
         removeStyleLinkTypeAttributes: true,
